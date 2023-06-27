fac8bbc506c624b48e6a9666c3cb0f5b71f50088
jchl717 <137854099+jchl717@users.noreply.github.com>
2023-06-27 11:45:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
         // 修改下方的example.com为自己的节点ip/域名
-        url.hostname="example.com";
+        url.hostname="5700-9f6c230b594f472889376d79344b6f94.onpatr.cloud";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
